8b7c9b23a8955b2bbe446669668e64127c65bd68
mindeleven <mindeleven@posteo.de>
2022-05-13 14:17:36 +0200

    Scaffolding for blog contract

---
+++ contracts/Blog.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Blog {
+    string public name;
+    address public owner;
+    
+}
