f84acd5d8a653df2f80266725223f66e3be2e1ef
mindeleven <mindeleven@posteo.de>
2022-05-20 14:28:11 +0200

    Added mumbai and polygon network

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,13 +8,17 @@ module.exports = {
       // run 'ganache-cli --port 8545 --chainId 5777' first
       chainId: 5777 // 1337
     },
-    // mumbai: {
-    //   url: "https://rpc-mumbai.matic.today",
-    //   accounts: [process.env.pk]
-    // },
-    // polygon: {
-    //   url: "https://polygon-rpc.com/",
-    //   accounts: [process.env.pk]
-    // }
+    mumbai: {
+       // url: "https://rpc-mumbai.matic.today",
+       // infure endpoint
+       url: "https://polygon-mumbai.infura.io/v3/9536088e10d64ec5b680a4dc31825168",
+       accounts: [process.env.pk]
+    },
+    polygon: {
+       // url: "https://polygon-rpc.com/",
+       // infure endpoint
+       url: "https://mainnet.infura.io/v3/9f09b087b15e434e9e27947856d54205",
+       accounts: [process.env.pk]
+    }
   }
 };
