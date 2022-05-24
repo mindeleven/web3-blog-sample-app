2acd17e9704c54230356d0a0e606cafa6a805199
mindeleven <mindeleven@posteo.de>
2022-05-24 10:20:39 +0200

    Redeployed contract address

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-    export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-    export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+    export const contractAddress = "0x024431db7f0222D24037F54ccdBe59d501ac741c"
+    export const ownerAddress = "0xDAF5660136D5b556F2649f08E1Ad2F54a2D161cb"
   
